e5d6e0047c2b77940a382c1e6b651ac2fda0454c
lamrelia <149579117+lamrelia@users.noreply.github.com>
2024-04-01 18:57:48 +0200

    Added first game object; food.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,47 @@
 #include <raylib.h>
 
+using namespace std;
+
+Color Red = {230, 41, 55, 255};
+
+int CellSize = 30;
+int CellCount = 25;
+
+class Food 
+{
+    public:
+        Vector2 position;
+
+        Food()
+        {
+            position = GenerateRandomPosition();
+        }
+
+        void Draw()
+        {
+            DrawCircle(position.x * CellSize, position.y * CellSize, CellSize / 2, Red);
+        }
+        
+        Vector2 GenerateRandomPosition()
+        {
+            float x = GetRandomValue(0, CellCount - 1);
+            float y = GetRandomValue(0, CellCount - 1);
+            return Vector2{x, y};
+        }
+
+};
+
 int main()
 {
-    InitWindow(750, 750, "Snake");
+    InitWindow(CellSize * CellCount, CellSize * CellCount, "Snake");
     SetTargetFPS(240);
+
+    Food food = Food();
 
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
+        food.Draw();
 
         EndDrawing();
     }
