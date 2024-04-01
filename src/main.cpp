4cbf1082060b1c99af155592aa2d72f6a4cb1c00
lamrelia <149579117+lamrelia@users.noreply.github.com>
2024-04-01 18:33:00 +0200

    Added window target framerate limit and empty game loop.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 int main()
 {
     InitWindow(750, 750, "Snake");
+    SetTargetFPS(240);
+
+    while(WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        EndDrawing();
+    }
 
     CloseWindow();
     return 0;
