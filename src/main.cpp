a7a17d6fc78f02e601f23b39915054505ec6b199
lamrelia <149579117+lamrelia@users.noreply.github.com>
2024-04-01 18:26:44 +0200

    Removed bloat, created window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,8 @@
 #include <raylib.h>
-#include "ball.h"
 
 int main()
 {
-    Color darkGreen = Color{20, 160, 133, 255};
-
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    Ball ball = Ball();
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
-        EndDrawing();
-    }
+    InitWindow(750, 750, "Snake");
 
     CloseWindow();
     return 0;
